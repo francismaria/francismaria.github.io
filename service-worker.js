758124a08b1770b418b8ea5adffeaa31051d8891
francismaria <franciscomaria97@hotmail.com>
2019-07-08 23:53:35 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.2bb91be48626ab96a6dd78e3cb61867e.js"
 );
 
 self.addEventListener('message', (event) => {
