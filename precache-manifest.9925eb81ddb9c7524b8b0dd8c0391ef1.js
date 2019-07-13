6cae0455425c1c6a9ca03ab3606c3b005cf23d3c
francismaria <franciscomaria97@hotmail.com>
2019-07-13 14:22:07 +0100

    Updates

--- precache-manifest.9925eb81ddb9c7524b8b0dd8c0391ef1.js
+++ precache-manifest.9925eb81ddb9c7524b8b0dd8c0391ef1.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "03a968b604896317e441e921098f440e",
+    "revision": "d0385f4116a16d323eca74012244145f",
     "url": "/index.html"
   },
   {
-    "revision": "a75c874d103cbcf4a8c3",
-    "url": "/static/css/main.8a4739de.chunk.css"
+    "revision": "05e9447927eb7c48c407",
+    "url": "/static/css/main.94bfed12.chunk.css"
   },
   {
     "revision": "9eee67cf782a38d35aeb",
     "url": "/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "a75c874d103cbcf4a8c3",
-    "url": "/static/js/main.213dfdad.chunk.js"
+    "revision": "05e9447927eb7c48c407",
+    "url": "/static/js/main.cd668859.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
