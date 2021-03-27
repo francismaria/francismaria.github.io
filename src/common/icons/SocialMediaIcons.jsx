8f1e2233c7d966fef9cf889d3267ca9bc0eb6ef8
Francisco Maria <franciscomaria97@hotmail.com>
2021-03-27 10:20:06 +0000

    Update medium link

--- src/common/icons/SocialMediaIcons.jsx
+++ src/common/icons/SocialMediaIcons.jsx
@@ -5,7 +5,7 @@ function SocialMediaIcons() {
     <ul className='social-media-icons list-opacity-animation'>
       <li><a href='https://www.linkedin.com/in/francismaria/' rel='noreferrer' target='_blank'><i className="fab fa-linkedin-in"></i></a></li>
       <li><a href='https://github.com/francismaria' rel='noreferrer' target='_blank'><i className="fab fa-github"></i></a></li>
-      <li><a href='https://medium.com/@francismariafms' rel='noreferrer' target='_blank'><i className="fab fa-medium-m"></i></a></li>
+      <li><a href='https://medium.com/@francismaria' rel='noreferrer' target='_blank'><i className="fab fa-medium-m"></i></a></li>
       <li><a href='mailto:franciscomaria97@hotmail.com' rel='noreferrer' target='_blank'><i className="fas fa-envelope"></i></a></li>
     </ul>
   );
